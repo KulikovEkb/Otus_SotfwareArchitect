4278ac62f97d65fb9be009f0de866512266c5936
kkmegarif@gmail.com <sp1922spartak>
2020-08-01 20:48:49 +0500

    Helm init

---
+++ SoftwareArchitect.Job/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoftwareArchitect.Job
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
