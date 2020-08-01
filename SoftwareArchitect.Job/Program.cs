f971be323e74ecfa7ccd8d6ab1f76921a70dcc3a
kkmegarif@gmail.com <sp1922spartak>
2020-08-01 23:57:54 +0500

    migration continued

--- SoftwareArchitect.Job/Program.cs
+++ SoftwareArchitect.Job/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SoftwareArchitect.Common.Models;
+using SoftwareArchitect.Storages.UserStorage;
+using SoftwareArchitect.Storages.UserStorage.Models;
 
 namespace SoftwareArchitect.Job
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static int Main()
         {
-            Console.WriteLine("Hello World!");
+            var optionsBuilder = new DbContextOptionsBuilder<UserContext>()
+                .UseNpgsql(Environment.GetEnvironmentVariable("POSTGRES_CONNECTION_STRING") ??
+                           throw new Exception("connection string is wrong"));
+            var userContext = new UserContext(optionsBuilder.Options);
+            var userStorage = new UserStorage(userContext);
+
+            var migrationUser = new User
+            {
+                Id = 5,
+                Username = "MigrationUser",
+                FirstName = "MigrationUserName",
+                LastName = "MigrationUserLastName",
+                Email = "migration@migration.com"
+            };
+
+            var migrateResult =
+                userStorage.CreateOrUpdateAsync(migrationUser).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            return migrateResult.IsSuccess ? 0 : 1;
         }
     }
 }
