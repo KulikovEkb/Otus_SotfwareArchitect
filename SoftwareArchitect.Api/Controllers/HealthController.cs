5797753da2aced94304f82e15bcfc0d801c8bdbe
kkmegarif@gmail.com <sp1922spartak>
2020-08-09 01:04:40 +0500

    prometheus client usage init

--- SoftwareArchitect.Api/Controllers/HealthController.cs
+++ SoftwareArchitect.Api/Controllers/HealthController.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using Prometheus;
 
 namespace SoftwareArchitect.Api.Controllers
 {
     public class HealthController : ControllerBase
     {
+        private static readonly Counter HealthRequestCounter = Metrics.CreateCounter(
+            "myapp_health_requests_total", "Total number of health requests");
+        
         [HttpGet("health")]
-        public IActionResult CheckHealth() => Ok(new {Status = "OK"});
+        public IActionResult CheckHealth()
+        {
+            HealthRequestCounter.Inc();
+            return Ok(new {Status = "OK"});
+        }
     }
 }
--- SoftwareArchitect.Api/Startup.cs
+++ SoftwareArchitect.Api/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Prometheus;
 using SoftwareArchitect.Storages.UserStorage;
 using SoftwareArchitect.Storages.UserStorage.Models;
 
@@ -31,8 +32,13 @@ namespace SoftwareArchitect.Api
         public void Configure(IApplicationBuilder app)
         {
             app.UseRouting();
+            app.UseHttpMetrics();
 
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapMetrics();
+            });
         }
     }
 }
